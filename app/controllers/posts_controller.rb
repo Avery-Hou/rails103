deff223b0ccd933b8f0c9da2311c4fdcda48634f
Avery Hou <avery.hou@gmail.com>
2019-03-31 23:29:45 +0800

    6-3 implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+	def new
+		@post = Post.new
+		@group = Group.find(params[:group_id])
+	end
+
+	def create
+		@group = Group.find(params[:group_id])
+		@post = Post.new(post_params)
+		@post.user = current_user
+		@post.group = @group
+		if @post.save
+			redirect_to group_path(@group), notice: "post create success"
+		else
+			render :new
+		end
+	end
+
+
+	private 
+	def post_params
+		params.require(:post).permit(:content)
+	end
 end
