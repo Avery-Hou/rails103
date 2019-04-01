eeebff148c7ccfba51a894487dfc24959641d22f
Avery Hou <avery.hou@gmail.com>
2019-04-01 11:00:57 +0800

    8-3 implement update/delete post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,20 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+		@group = Group.find(params[:group_id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+      redirect_to account_posts_path
+		else
+      render :edit
+		end
+	end
+
 	def destroy
 		@post = Post.find(params[:id])
 		@post.destroy
